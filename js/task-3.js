58757d937719bb9bc1ac54232c584ea02c35257d
Viktor Datii <datiyvk@gmail.com>
2024-12-21 18:06:38 +0200

    js-hw-8

--- js/task-3.js
+++
@@ -1,8 +0,0 @@
-const inputName = document.querySelector("input#name-input");
-const outputName = document.querySelector("span#name-output");
-
-inputName.addEventListener("input", () => {
-    outputName.textContent = inputName.value.trim() || "Anonymous";
-});
-
-inputName.classList.add("input-style");
